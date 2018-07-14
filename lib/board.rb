e4f88270635136cbf3bdea83e9a34e6a5971e8b3
Harper Bellows <harper.bellows@gmail.com>
2018-07-14 17:07:52 -0600

    make game board

---
+++ lib/board.rb
@@ -0,0 +1,17 @@
+class Board
+  attr_reader :grid
+
+  def initialize
+    @grid = {
+      'A' => [],
+      'B' => [],
+      'C' => [],
+      'D' => [],
+      'E' => [],
+      'F' => [],
+      'G' => []
+      }
+
+  end
+
+end

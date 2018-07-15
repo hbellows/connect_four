8d527ef55be8c6c16759d4a516921c325ee83a06
Harper Bellows <harper.bellows@gmail.com>
2018-07-14 18:48:37 -0600

    initial commit, add functionality for message center class

---
+++ lib/message_center.rb
@@ -0,0 +1,25 @@
+class MessageCenter
+  # def initialize
+  # end
+  #
+  def welcome
+    'Connect Four is a classic board game where players take turns trying to'
+    'create a line of four of their own pieces without being blocked by their'
+    'opponent.'
+    "\n"
+    'The rules are simple:'
+    'At the prompt each player, in turn, will choose a column in which to'
+    'drop their chip. Player one = "X", Player two = "O".'
+    "\n"
+    'The game ends when one of the two players successfully lines up four of'
+    'their pieces horizontally, vertically, or diagonally.'
+    "\n"
+    'The first to player to line up for chips wins the round. The game ends'
+    'in a draw if the 7-column, 6-row grid is filled without either player'
+    'successfully connecting four.'
+  end
+end
+
+
+
+  # puts '-' * 75

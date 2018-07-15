bd9a275a58b56cfed24c87aa0621243ffb151a6e
Harper Bellows <harper.bellows@gmail.com>
2018-07-15 06:35:51 -0600

    wip

--- test/game_center_test.rb
+++ test/game_center_test.rb
@@ -10,4 +10,16 @@ class GameCenterTest < Minitest::Test
     assert_instance_of GameCenter, game_center
   end
 
+  def test_it_can_take_in_and_sanitize_user_input
+    game_center = GameCenter.new
+
+    assert_equal "G", game_center.sanitize('g')
+  end
+
+  def test_it_can_detect_invalid_user_input
+    game_center = GameCenter.new
+
+    assert_equal false, game_center.input_valid?('z')
+  end
+
 end

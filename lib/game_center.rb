487d5371930abe01cad1639c9991817d39ce7525
Harper Bellows <harper.bellows@gmail.com>
2018-07-15 14:12:50 -0600

    move validation message to message center module

--- lib/game_center.rb
+++ lib/game_center.rb
@@ -1,21 +1,32 @@
 class GameCenter
 
+  include MessageCenter
+
+  def initialize
+    @board = Board.new
+  end
+
   def sanitize(input)
-    clean_input = input.upcase
+    cleaned_input = input.upcase
   end
 
-  def input_valid?(clean_input)
-    acceptable_input = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
-    acceptable_input.include?(clean_input)
-    # require "pry"; binding.pry
+  def input_valid?(cleaned_input)
+    acceptable_input = @board.grid.keys
+    acceptable_input.include?(cleaned_input)
   end
 
-  def validate_input(clean_input)
-    if input_valid?(clean_input)
-      "You have choosen column #{clean_input}."
+  def validate_input(cleaned_input)
+    if input_valid?(cleaned_input)
+      "You have choosen column #{cleaned_input}."
     else
-      'The letter you have entered is invalid.  Please enter a letter from "A" to "G".'
+      #can I run a validation loop here?
+
     end
+  end
+
+  def validation_loop
+    unless input_valid?(cleaned_input)
+      invalid_enrty
   end
 
 

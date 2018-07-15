ce76b9f5359919e932bd121ee531c7bfad9e5301
Harper Bellows <harper.bellows@gmail.com>
2018-07-14 18:48:14 -0600

    wip

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -1,22 +1,7 @@
 require_relative './board'
 board = Board.new
 
-puts '-' * 75
-puts 'Connect Four is a classic board game where players take turns trying to'
-puts 'create a line of four of their own pieces without being blocked by their'
-puts 'opponent.'
-puts "\n"
-puts 'The rules are simple:'
-puts 'At the prompt each player, in turn, will choose a column in which to'
-puts 'drop their chip. Player one = "X", Player two = "O".'
-puts "\n"
-puts 'The game ends when one of the two players successfully lines up four of'
-puts 'their pieces horizontally, vertically, or diagonally.'
-puts "\n"
-puts 'The first to player to line up for chips wins the round. The game ends'
-puts 'in a draw if the 7-column, 6-row grid is filled without either player'
-puts 'successfully connecting four.'
-puts '-' * 75
+
 puts board.display_top_line.join
 puts board.display_grid
 puts "\n"

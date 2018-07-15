73c40958866e1d870e947164c66cb5f30637c591
Harper Bellows <harper.bellows@gmail.com>
2018-07-14 19:42:37 -0600

    wip

--- test/message_center_test.rb
+++ test/message_center_test.rb
@@ -15,15 +15,22 @@ class MessageCenterTest < Minitest::Test
     message_center = MessageCenter.new
 
     expected =
-    "Connect Four is a classic board game where players take turns trying to\n
-    create a line of four of their own pieces without being blocked by their\n
-    opponent.\n\nThe rules are simple:\n At the prompt each player, in turn,
-    will choose a column in which to drop their chip.\n Player one = 'X',
-    Player two = 'O'.\n The game ends when one of the two players successfully
-    lines up four of\n their pieces horizontally, vertically, or diagonally.\n
-    The first to player to line up for chips wins the round. The game ends\n
-    in a draw if the 7-column, 6-row grid is filled without either player\n
-    successfully connecting four.\n"
+"Connect Four is a classic board game where players take turns trying to
+create a line of four of their own pieces without being blocked by their
+opponent.
+
+The rules are simple:
+At the prompt each player, in turn, will choose a column in which to drop
+their chip.
+
+The game ends when one of the two players successfully lines up four of
+their pieces horizontally, vertically, or diagonally.
+
+The first to player to line up for chips wins the round. The game ends
+in a draw if the 7-column, 6-row grid is filled without either player
+successfully connecting four.
+
+Player one = 'X', Player two = 'O'."
 
     assert_equal expected, message_center.welcome
   end

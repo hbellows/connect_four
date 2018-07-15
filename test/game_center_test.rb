e8ed0d397e9c55a81474bebc2946c841b0ac4a56
Harper Bellows <harper.bellows@gmail.com>
2018-07-15 10:18:03 -0600

    add tests for validation of user input and tests creating validation/invalidation messages

--- test/game_center_test.rb
+++ test/game_center_test.rb
@@ -10,7 +10,7 @@ class GameCenterTest < Minitest::Test
     assert_instance_of GameCenter, game_center
   end
 
-  def test_it_can_take_in_and_sanitize_user_input
+  def test_it_can_sanitize_and_prepare_user_input
     game_center = GameCenter.new
 
     assert_equal "G", game_center.sanitize('g')
@@ -19,7 +19,46 @@ class GameCenterTest < Minitest::Test
   def test_it_can_detect_invalid_user_input
     game_center = GameCenter.new
 
-    assert_equal false, game_center.input_valid?('z')
+    clean_input = game_center.sanitize('z')
+    assert_equal false, game_center.input_valid?(clean_input)
   end
+
+  def test_it_can_detect_valid_user_input
+    game_center = GameCenter.new
+
+    clean_input = game_center.sanitize('a')
+    assert_equal true, game_center.input_valid?(clean_input)
+  end
+
+  def test_it_can_generate_an_input_invalidation_message
+    game_center = GameCenter.new
+
+    input = 'z'
+
+    clean_input = game_center.sanitize(input)
+    game_center.input_valid?(clean_input)
+
+    expected = 'The letter you have entered is invalid.  Please enter a letter from "A" to "G".'
+
+    assert_equal expected, game_center.validate_input(clean_input)
+  end
+
+  def test_it_can_generate_an_input_validation_message
+    game_center = GameCenter.new
+
+    input = 'a'
+
+    clean_input = game_center.sanitize(input)
+    game_center.input_valid?(clean_input)
+
+    expected = "You have choosen column A."
+
+    assert_equal expected, game_center.validate_input(clean_input)
+  end
+
+  def test_it_can_translate_valid_input_to_
+
+  end
+
 
 end

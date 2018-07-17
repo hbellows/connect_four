2cb34bc28022a1b93361ccc3871000c1842cd2ef
Harper Bellows <harper.bellows@gmail.com>
2018-07-17 06:59:10 -0600

    refactor game play loop; rename file from connect_four_runner to play_game

---
+++ lib/play_game.rb
@@ -0,0 +1,38 @@
+require './lib/board'
+require './lib/game_center'
+require './lib/message_center'
+
+class ConnectFour
+  include MessageCenter
+
+  def initialize
+    @board = Board.new
+  end
+  #
+  # def intro
+  #   pretty_line
+  #   welcome
+  #   @board.repl
+  #   puts "\n"
+  #   game_center = GameCenter.new
+  #   game_center.player_one_turn
+  # end
+  def start
+    play_loop
+  end
+  
+  def play_loop
+    until player_win? || computer_win?
+      repl
+      puts `clear`
+      sleep 0.5
+      place_computer_piece('C')
+      puts `clear`
+      repl
+      place_player('P')
+      puts `clear`
+      repl
+    end
+  end
+
+end

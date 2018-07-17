ee9f20f529f0794d07555c2ca18cfe6d56d41ec3
Harper Bellows <harper.bellows@gmail.com>
2018-07-17 07:08:28 -0600

    refactor player loop

--- lib/player.rb
+++ lib/player.rb
@@ -5,8 +5,23 @@ class Player
     @moves = []
   end
 
-  def saved_moves(valid_move)
-    @moves << valid_move
+  def place_player(player, input=nil)
+    input = gets.chomp.upcase
+    print '> '
+    exit_message if quit_commands(input)
+    validation_loop(player, input)
+  end
+
+  def input_valid?(input)
+    return true if i.keys.include?(input)
+  end
+
+  def validation_loop(player, input)
+    unless input_valid?(input)
+      puts 'Please choose a letter between A and G'
+      place_player(player, input)
+    end
+    place(player, input)
   end
 
 

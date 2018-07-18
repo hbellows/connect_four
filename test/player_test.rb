b93182fcb120276a5f1f34963a075f66240dfcc4
Harper Bellows <harper.bellows@gmail.com>
2018-07-18 08:29:04 -0600

    add tests for placing player pieces

--- test/player_test.rb
+++ test/player_test.rb
@@ -5,10 +5,36 @@ require './lib/board'
 
 class PlayerTest < Minitest::Test
 
-  def test_it_exists
-    player = Player.new
+  def setup
+    @board = Board.new
+    @player = Player.new(@board)
+  end
 
-    assert_instance_of Player, player
+  def test_it_exists
+    assert_instance_of Player, @player
+  end
+
+  def test_it_can_validate_input
+    assert_equal true, @player.input_valid?('A')
+  end
+
+  def test_it_can_determine_if_the_column_is_not_full
+    @player.place('B')
+
+    assert_equal false, @player.full('B')
+  end
+
+  def test_it_can_determine_if_the_column_is_full
+    6.times do
+      @player.place('A')
+    end
+
+    assert_equal true, @player.full('A')
+  end
+
+  def test_it_can_place_player_piece
+    assert_equal 'X', @player.place('A')
+    assert_equal 'X', @player.place('B')
   end
 
 end
